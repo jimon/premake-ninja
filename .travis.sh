1a1c93357004167122f7ef28a0b1e2242922ecae
Dmytro Ivanov <jimon.j1m0n@gmail.com>
2015-11-06 23:34:35 +0100

    remove broken travis stuff

--- .travis.sh
+++ .travis.sh
@@ -29,11 +29,3 @@ chmod +x ninja
 ls -al
 
 popd
-
-#if [ "$TRAVIS_OS_NAME" == "linux" ]; then
-#  sudo apt-get update
-#  sudo apt-get python3
-#elif [ "$TRAVIS_OS_NAME" == "osx" ]; then
-#  brew update
-#  brew install python3
-#fi
